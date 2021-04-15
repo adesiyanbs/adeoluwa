abf031332bbcdbcaa4152120bdfe90e3af03bf14
Adeoluwa <minneade@gmail.com>
2021-04-15 22:36:22 +0100

    others

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,18 +1,25 @@
 import React from 'react'
+import '../styles/contact.css'
+import git from '../img/git.png'
+import twitter from '../img/twitter.png'
+
 
 function Contact() {
     return (
         <div>
             <section className="cnt">
                 <div className="cnt_container">
-                    <h1>Let's Work</h1>
-                    <p>I'm currently open to any freelancing work available,
-                         to get in touch send a mail and we'll begin work!
+                    <div className="cnt_text">
+                        <h1>Let's <span className="work">Work</span></h1>
+                        <p>I'm currently open to any freelancing work available,
+                        to get in touch send a mail and we'll begin work!
                     </p>
-                    <div className="socials">
-                       
                     </div>
-                    <a href="minneade@gmail.com">say hello</a>
+                    <div className="socials">
+                        <a href="https://github.com/adesiyanbs"><img src={git} alt="github profile"/></a>
+                        <a href="https://twitter.com/commodore_ade"><img src={twitter} alt=" twitter account"/></a>
+                    </div>
+                    <a className="hello" href="mailto:minneade@gmail.com">Mail me!</a>
                 </div>
             </section>
         </div>
