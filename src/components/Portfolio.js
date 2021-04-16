b1eabd199d1498012eb5232af555a392e242b9b8
Adeoluwa <minneade@gmail.com>
2021-04-16 16:15:21 +0100

    port

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -4,7 +4,40 @@ import '../styles/portfolio.css'
 function Portfolio() {
     return (
         <div>
-            
+            <section className="port">
+                <h1>My Projects</h1>
+                <div className="port-container">
+                    <div className="port-cards">
+                        <div className="top"></div>
+                        <div className="bot">
+                            <h4>PORTFOLIO 1</h4>
+                            <button>Check It</button>
+                        </div>
+                    </div>
+                    <div className="port-cards">
+                        <div className="top"></div>
+                        <div className="bot">
+                            <h4>PORTFOLIO 2</h4>
+                            <button>Check It</button>
+                        </div>
+                    </div>
+                    <div className="port-cards">
+                        <div className="top"></div>
+                        <div className="bot">
+                            <h4>PORTFOLIO 3</h4>
+                            <button>Check It</button>
+                        </div>
+                    </div>
+                    <div className="port-cards">
+                        <div className="top"></div>
+                        <div className="bot">
+                            <h4>PORTFOLIO 4</h4>
+                            <button>Check It</button>
+                        </div>
+                    </div>
+                    
+                </div>
+            </section>
         </div>
     )
 }
